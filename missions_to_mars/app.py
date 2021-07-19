0718b311f4658bfba8902e9b8633f0d720436f87
DataTortuga <dcurtis390@gmail.com>
2021-07-19 15:38:14 -0500

    Edited app.py

--- missions_to_mars/app.py
+++ missions_to_mars/app.py
@@ -5,24 +5,30 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
 mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/phone_app")
 
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    mars_nws = mongo.db.mars_nws.find_one()
+    return render_template("index.html", mars_nws=mars_nws)
 
 
 @app.route("/scrape")
 def scraper():
-    mars_db = mongo.db.mars_db
+
+    mars_nws = mongo.db.mars_nws
+    mars_f_img = mongo.db.mars_f_img
+    mars_tb = mongo.db.mars_tb
+    mars_hem = mongo.db.mars_hem
+
     mars_data = scrape_mars.scrape()
-    mars_db.update({}, mars_data, upsert=True)
+    mars_nws.update({}, mars_data[0], upsert=True)
+    mars_f_img.update({}, mars_data[1], upsert=True)
+    mars_tb.update({}, mars_data[2], upsert=True)
+    mars_hem.update({}, mars_data[3], upsert=True)
+    
     return redirect("/", code=302)
 
 
